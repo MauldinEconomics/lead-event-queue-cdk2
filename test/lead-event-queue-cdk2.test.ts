ae96025374f750e1ab8ecee6d1262223dfd62a7a
Gjorge Mihailov <gmihailov@mauldineconomics.com>
2022-06-04 03:06:21 +0200

    Initial commit

---
+++ test/lead-event-queue-cdk2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LeadEventQueueCdk from '../lib/lead-event-queue-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lead-event-queue-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LeadEventQueueCdk.LeadEventQueueCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
